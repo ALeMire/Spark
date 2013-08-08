2a33fa6fd6058acef60fec221ca76f30ec05992e
ALeMire <aaronlemirerva@gmail.com>
2013-08-07 21:01:07 -0400

    README.md updated. added spark.sh

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Spark
 =====
+
+A small VPS startup script for Redhat-based systems.
---
+++ spark.sh
@@ -0,0 +1,7 @@
+##### Spark.sh -- a setup script for new VPSs #####
+#                                                 #
+#  Spark.sh is written for Redhat-based systems.  #
+#  Want it for your distro? Send a pull request.  #
+###################################################
+
+#!/bin/bash
